c4c74efda5130b55df230e369a269696a53976ac
Salomon Franco <110070841+SalomonFranco@users.noreply.github.com>
2023-06-17 19:52:03 +0000

    logicWorkingProperly

--- src/js/icon.js
+++ src/js/icon.js
@@ -1,6 +1,7 @@
-import fontawesome from '@fortawesome/fontawesome';
-import faClock from '@fortawesome/fontawesome-free-regular/faClock';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Clock } from '@fortawesome/fontawesome-free-regular/Clock';
+
 fontawesome.config = {
   autoReplaceSvg: 'nest'
 };
-fontawesome.library.add(faClock);
+fontawesome.library.add(Clock);
--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,19 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "prop-types"
+import Card from './component/card.jsx'
 
 // include your styles into the webpack bundle
-import './icon.js'
+import "../styles/index.css";
 
-//render your react application
-function SimpleCounter(props){
-    return(<div className="bigCounter"> 
-    <div className="calendar">
-    <i className="far fa-clock"></i> 
-    </div>
-    <div className="four">{props.digitFour}</div> 
-    <div className="three">{props.digitThree}</div> 
-    <div className="two">{props.digitTwo}</div> 
-    <div className="one">{props.digitOne}</div>
-    </div>);
-}
-SimpleCounter.propTypes = {
-    digitFour: PropTypes.number,
-    digitThree: PropTypes.number,
-    digitTwo: PropTypes.number,
-    digitOne: PropTypes.number,
-};
+// //import your own components
+// import Home from "./component/home.jsx";
 
-let counter = 0;
-setInterval(function(){
-    const four = Math.floor(counter/1000)
-    const three = Math.floor(counter/100)
-    const two = Math.floor(counter/10)
-    const one = Math.floor(counter/1)
-    console.log(four, three, two, one)
-    //render your react
-    counter++;
-    },1000);
-    ReactDOM.render(<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />, document.querySelector("#app") 
-    );
+// //render your react application
+// ReactDOM.render(<Home />, document.querySelector("#app"));
+//import react into the bundle
+let secondsPassed = 0;
+setInterval(() => {
+    secondsPassed++;
+    ReactDOM.render(<Card seconds={secondsPassed} />, document.querySelector("#app"));
+}, 1000);
